fb3baeb1d58b9e2d43f94a9d2301b010cbfc1672
joysi <joysingh.js1@gmail.com>
2023-04-28 09:44:08 +1000

    Update...

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 # List of all countries in the world
-countries = [
-    'Afghanistan', 'Albania', 'Algeria', ...
-    # Add all the countries here
-]
+countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia',
+             'Australia', 'Austria', 'Azerbaijan', 'The Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus',
+             'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil',
+             'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada',
+             'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros',
+             'Democratic Republic of the Congo', 'Republic of the Congo', 'Costa Rica', "Côte d'Ivoire", 'Croatia',
+             'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic',
+             'East Timor (Timor-Leste)', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia',
+             'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'The Gambia', 'Georgia', 'Germany', 'Ghana',
+             'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary',
+             'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan',
+             'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kosovo', 'Kuwait',
+             'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania',
+             'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands',
+             'Mauritania', 'Mauritius', 'Mexico', 'Micronesia, Federated States of', 'Moldova', 'Monaco', 'Mongolia',
+             'Montenegro', 'Morocco', 'Mozambique', 'Myanmar (Burma)', 'Namibia', 'Nauru', 'Nepal', 'Netherlands',
+             'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Palau',
+             'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania',
+             'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa',
+             'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone',
+             'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka',
+             'Sudan', 'South Sudan', 'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania',
+             'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu',
+             'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan',
+             'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']
+
 
 # Initialize the web driver
 driver = webdriver.Edge()
@@ -15,17 +38,14 @@ driver = webdriver.Edge()
 # Navigate to the quiz website
 driver.get('https://www.sporcle.com/games/g/world')
 
-# Identify and click the button element
-button_element = driver.find_element(by=By.ID, value='button_play')
-button_element.click()
-
 
 # Wait for Edge to finish loading the webpage
-def web_page_loaded(driver):
-    return driver.execute_script("return initialized")
+driver.implicitly_wait(10)
+# input("Is the webpage loaded?")
 
-
-WebDriverWait(driver, timeout=10).until(web_page_loaded)
+# Identify and click the button element
+button_element = driver.find_element(by=By.ID, value='button-play')
+button_element.click()
 
 # Identify the input element on the quiz web page
 input_element = driver.find_element(by=By.ID, value='gameinput')
@@ -35,7 +55,11 @@ for country in countries:
     input_element.clear()
     input_element.send_keys(country)
     input_element.send_keys(Keys.RETURN)
-    time.sleep(1)  # Add a delay to avoid being blocked or flagged as a bot
+    # time.sleep(1)  # Add a delay to avoid being blocked or flagged as a bot
 
-# Close the browser
-# driver.quit()
+# DEBUG: Check which countries did I miss
+
+input('proceed?')
+
+# Close the browser.
+driver.quit()
